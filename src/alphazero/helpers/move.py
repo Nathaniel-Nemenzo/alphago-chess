c915f9843dcac1305fd09bbceb9e00a86392b52a
Nathaniel Nemenzo <natnemenzo1@gmail.com>
2023-02-25 17:35:12 -0600

    board representation

--- src/alphazero/helpers/move.py
+++ src/alphazero/helpers/move.py
@@ -1,3 +1,13 @@
 """
 Encapsulates classes for Board encoding and decoding in AlphaZero move representation
 """
+
+class MoveRepresentation:
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
--- src/alphazero/run.py
+++ src/alphazero/run.py
@@ -1,8 +1,6 @@
 """
 Main entry point for running from command line
 """
-
-
 if __name__ == "__main__":
     import manager
     manager.start()
